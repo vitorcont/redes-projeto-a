4f93b4e6ee622128053d443e5bca3d87abc1ad5a
Renato Santos <404renato@gmail.com>
2022-04-01 09:35:11 -0300

    Server upgrades

--- server.py
+++ server.py
@@ -1,34 +1,38 @@
 import sys
 import socket
-# import json
-# from datetime import datetime
-# import pytz
+from datetime import datetime
+import pytz
 
 ip = sys.argv[1]
 port = int(sys.argv[2])
 
+port_aux = False
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 server = (ip, port)
 udp.bind(server)
 
-# ipOrigem = ip
-# ipDestino = ip(?)
-# portOrigem = udp.getsockname()[1]
-# portDestino = port
-# ----------------------------------------------
-# TimestampOrigem e Destino:
-# tz_SP = pytz.timezone('America/Sao_Paulo')
-# datetime_SP = datetime.now(tz_SP)
-
-# current_time = datetime_SP.strftime("%H:%M:%S")
+tz_SP = pytz.timezone('America/Sao_Paulo')
+datetime_SP = datetime.now(tz_SP)
 
 while True:
     msg_recv, end_client = udp.recvfrom(1024)
-    print(f"Client {end_client}: ", msg_recv.decode('utf-8'))
+    # Timestamp de recebimento
+    current_time = datetime_SP.strftime("%H:%M:%S")
+    print(f"Client {end_client}: ", msg_recv.decode('utf-8'), current_time)
 
     msg = input()
-    udp.sendto(bytes(msg, "utf8"), end_client)
-    udp.sendfile('./teste.json', offset=0, count=1)
+
+    current_time = datetime_SP.strftime("%H:%M:%S")
+
+    json = {
+        'Ip_Origem': ip,
+        'Ip_Destino': end_client[0],
+        'Porta_Origem': port,
+        'Porta_Destino': udp.getsockname()[1] if port_aux != False else 8080,
+        'Timestamp': current_time,
+        'Mensagem': msg,
+    }
+    port_aux = True
+    udp.sendto(bytes(str(json), "utf8"), end_client)
 udp.close()
