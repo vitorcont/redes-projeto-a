9af753ce80be2fc3a8aacb17c7cdc27f4e504fa3
Vitor Conti <69795902+vitorcont@users.noreply.github.com>
2022-04-02 22:24:29 -0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,39 @@
+import sys
+import socket
+from datetime import datetime
+import pytz
+
+port_aux = False
+
+ip = sys.argv[1]
+port = int(sys.argv[2])
+
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+destiny = (ip, port)
+
+tz_SP = pytz.timezone('America/Sao_Paulo')
+datetime_SP = datetime.now(tz_SP)
+
+while True:
+    msg = input()
+    current_time = datetime_SP.strftime("%H:%M:%S")
+
+    json = {
+        'Ip_Origem': socket.gethostbyname(socket.gethostname()),
+        'Ip_Destino': ip,
+        'Porta_Origem': 8080 if port_aux == True else 1234,
+        'Porta_Destino': port,
+        'Timestamp': current_time,
+        'Mensagem': msg
+    }
+    udp.sendto(bytes(str(json), "utf8"), destiny)
+
+    port_aux = True
+
+    msg_recv, end_client = udp.recvfrom(1024)
+    # Timestamp de recebimento
+    current_time = datetime_SP.strftime("%H:%M:%S")
+    print("Recebi = ", msg_recv, " , Do cliente",
+          end_client, ' Timestamp Resposta: ', current_time)
+
+udp.close()
---
+++ server.py
@@ -0,0 +1,38 @@
+import sys
+import socket
+from datetime import datetime
+import pytz
+
+ip = sys.argv[1]
+port = int(sys.argv[2])
+
+port_aux = False
+
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server = (ip, port)
+udp.bind(server)
+
+tz_SP = pytz.timezone('America/Sao_Paulo')
+datetime_SP = datetime.now(tz_SP)
+
+while True:
+    msg_recv, end_client = udp.recvfrom(1024)
+    # Timestamp de recebimento
+    current_time = datetime_SP.strftime("%H:%M:%S")
+    print(f"Client {end_client}: ", msg_recv.decode('utf-8'), ' Timestamp Resposta: ', current_time)
+
+    msg = input()
+
+    current_time = datetime_SP.strftime("%H:%M:%S")
+
+    json = {
+        'Ip_Origem': ip,
+        'Ip_Destino': end_client[0],
+        'Porta_Origem': port,
+        'Porta_Destino': udp.getsockname()[1] if port_aux != False else 8080,
+        'Timestamp': current_time,
+        'Mensagem': msg,
+    }
+    port_aux = True
+    udp.sendto(bytes(str(json), "utf8"), end_client)
+udp.close()
