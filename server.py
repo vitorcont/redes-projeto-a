6e4153525a5ba8708567726df3458df4b8ad9f24
vitor <vitorconti18@gmail.com>
2022-04-02 23:38:13 -0300

    done some changes

--- server.py
+++ server.py
@@ -2,9 +2,12 @@ import sys
 import socket
 from datetime import datetime
 import pytz
+from ast import literal_eval
 
-ip = sys.argv[1]
-port = int(sys.argv[2])
+print('DIGITE O IP DO SERVER: ')
+ip = input()
+print('DIGITE A PORTA: ')
+port = int(input())
 
 port_aux = False
 
@@ -18,21 +21,31 @@ datetime_SP = datetime.now(tz_SP)
 while True:
     msg_recv, end_client = udp.recvfrom(1024)
     # Timestamp de recebimento
-    current_time = datetime_SP.strftime("%H:%M:%S")
-    print(f"Client {end_client}: ", msg_recv.decode('utf-8'), ' Timestamp Resposta: ', current_time)
-
+    current_recieved = datetime_SP.strftime("%H:%M:%S")
+    print(f"Client {end_client}: ", msg_recv.decode('utf-8'))
+    
+    # send ack
+    msg_recv = msg_recv.decode("utf8")
+    msg_recv = literal_eval(msg_recv)
+    msg_recv['Timestamp Resposta'] = current_recieved
+    msg_recv['Ack'] = True
+    udp.sendto(bytes(str(msg_recv), "utf8"), end_client)
+    
+    # response msg to client
+    print('DIGITE O IP DO CLIENTE: ')
+    ipclient = input()
+    print('DIGITE A PORTA: ')
+    portclient = int(input())
     msg = input()
 
     current_time = datetime_SP.strftime("%H:%M:%S")
-
-    json = {
+    payload = {
         'Ip_Origem': ip,
-        'Ip_Destino': end_client[0],
+        'Ip_Destino': ipclient,
         'Porta_Origem': port,
-        'Porta_Destino': udp.getsockname()[1] if port_aux != False else 8080,
+        'Porta_Destino': portclient,
         'Timestamp': current_time,
         'Mensagem': msg,
     }
-    port_aux = True
-    udp.sendto(bytes(str(json), "utf8"), end_client)
+    udp.sendto(bytes(str(payload), "utf8"), (ipclient,portclient))
 udp.close()
